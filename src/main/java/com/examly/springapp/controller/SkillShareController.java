db3add987944c12f13bc0a3560827af5164b13f2
HarikrishnanChockalingam <harikrishnanchockkalingam2005@gmail.com>
2025-10-28 08:55:11 +0530

    Update SkillShareController.java

--- src/main/java/com/examly/springapp/controller/SkillShareController.java
+++ src/main/java/com/examly/springapp/controller/SkillShareController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/skillshares")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class SkillShareController {
     
     @Autowired
